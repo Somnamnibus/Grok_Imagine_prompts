23aff0225f2b9a623254695e010812d1d684a8e2
DEV-COM <kwp@tlen.pl>
2026-02-21 13:52:54 +0100

    update user prompt and enhance response parameters in OpenAI chat client

--- test3.py
+++ test3.py
@@ -10,7 +10,12 @@ response = client.chat.completions.create(
     model="gpt-4.1-nano",
     messages=[
         {"role": "system", "content": "Jesteś koderem w pythonie"},
-        {"role": "user", "content": "Napisz co wiesz o Pythonie w wersji 3.14" }
+        {"role": "user", "content": "Napisz mi funkcje unitest" }
     ],
+    temperature=0.5,
+    max_tokens=500,
+    top_p=0.9,
+    frequency_penalty=0.1
 )
 print(response.choices[0].message.content)
+print("Ilość tokenów", response.usage)
